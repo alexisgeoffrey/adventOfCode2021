8e4a24ca93fb279839ad33923e741e3c37ae1395
alexisgeoffrey <alexisgeoffrey1@gmail.com>
2021-12-13 04:41:01 -0600

    clippy fixes

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -9,13 +9,13 @@ fn main() {
         crab_fuel(FILE_PATH),
         crab_fuel_accel(FILE_PATH)
     );
-    println!("Experiment: {}", crab_fuel_experiment(FILE_PATH))
+    println!("Experiment: {}", crab_fuel_experiment(FILE_PATH));
 }
 
 pub fn crab_fuel(file_path: &str) -> i32 {
     let v = read_to_string(file_path)
         .unwrap()
-        .split(",")
+        .split(',')
         .map(|crab| crab.parse().unwrap())
         .sorted()
         .collect::<Vec<i32>>();
@@ -28,22 +28,21 @@ pub fn crab_fuel_experiment(file_path: &str) -> i32 {
     // VERY SLOW
     read_to_string(file_path)
         .unwrap()
-        .split(",")
+        .split(',')
         .map(|crab| crab.parse().unwrap())
         .sorted()
         .map(|crab: i32| {
             (read_to_string(file_path)
                 .unwrap()
-                .split(",")
+                .split(',')
                 .map(|crab| crab.parse().unwrap())
                 .sorted()
                 .collect::<Vec<i32>>()[read_to_string(file_path)
                 .unwrap()
-                .split(",")
-                .map(|crab| crab.parse().unwrap())
+                .split(',')
+                .map::<i32,_>(|crab| crab.parse().unwrap())
                 .sorted()
-                .collect::<Vec<i32>>()
-                .len()
+                .count()
                 / 2]
                 - crab)
                 .abs()
@@ -54,20 +53,20 @@ pub fn crab_fuel_experiment(file_path: &str) -> i32 {
 pub fn crab_fuel_accel(file_path: &str) -> i32 {
     let v = read_to_string(file_path)
         .unwrap()
-        .split(",")
+        .split(',')
         .map(|x| x.parse().unwrap())
         .sorted()
         .collect::<Vec<i32>>();
 
     let (avg1, avg2) = {
-        let avg = v.iter().sum::<i32>() as f32 / v.len() as f32;
-        (avg.trunc(), avg.ceil())
+        let avg = f64::from(v.iter().sum::<i32>()) / v.len() as f64;
+        (avg.trunc() as i32, avg.ceil() as i32)
     };
 
     let calc_fuel = |avg| {
         v.iter()
             .map(|crab| {
-                (0..=(cmp::max(avg as i32, *crab) - cmp::min(avg as i32, *crab))).sum::<i32>()
+                (0..=(cmp::max(avg, *crab) - cmp::min(avg, *crab))).sum::<i32>()
             })
             .sum()
     };
